4369598dbc5fa8627005669b3b907d5683776162
tiganasari <tigana.sari@gmail.com>
2021-04-18 21:27:08 +0100

    Remove forecast

--- src/index.js
+++ src/index.js
@@ -58,16 +58,3 @@ form.addEventListener("submit", submitCity);
 
 search("London");
 
-
-function showCurrentCity(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
-let currentCity = document.querySelector("#my-city");
-currentCity.addEventListener("click", showCurrentCity);
-
-function searchLocation(position) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayTemperature);
-}
