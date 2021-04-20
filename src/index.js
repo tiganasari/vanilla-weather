06b7e389f08d408f26dfcab884c7f5c8c1187622
tiganasari <tigana.sari@gmail.com>
2021-04-20 10:57:53 +0100

    Added forecast JS template

--- src/index.js
+++ src/index.js
@@ -22,6 +22,28 @@ let today = `${day}, ${hours}:${minutes}`;
 let todayTime = document.querySelector("h2");
 todayTime.innerHTML = today;
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">` ;
+  days.forEach(function (day) {
+    forecastHTML = forecastHTML + ` 
+                        <div class="col-2">
+                            <div class="weater-forecast-date">${day} </div>
+                            <img src="http://openweathermap.org/img/wn/50d@2x.png" alt="" width="36" />
+                            <div class="weather-forecast-temperatures">
+                                <span class="weather-forecast-temperature-max"> 18° </span>
+                                <span class="weather-forecast-temperature-min"> 12° </span>
+                            </div>
+                        </div>`;
+  });
+  
+forecastHTML = forecastHTML + `</div>`;
+forecastElement.innerHTML = forecastHTML;
+console.log(forecastHTML)
+;}
+
 function displayTemperature(response) {
     document.querySelector("#city-search").innerHTML = response.data.name;
   document.querySelector("#showTemperature").innerHTML = `${Math.round(
@@ -90,3 +112,4 @@ celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 
 search("London");
+displayForecast();
